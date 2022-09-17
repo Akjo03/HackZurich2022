b1e7135360dfb477b3b9be70cb20006f8dcfd72f
Lukas Küffer <lukas.kueffer@outlook.com>
2022-09-17 03:18:53 +0200

    Additional build settings

--- HackZurichApp/src/main/java/ch/hackzurich/trains/MainActivity.kt
+++ HackZurichApp/src/main/java/ch/hackzurich/trains/MainActivity.kt
@@ -2,10 +2,12 @@ package ch.hackzurich.trains
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        OpenCVLoader.initDebug()
     }
 }

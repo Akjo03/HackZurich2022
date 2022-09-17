054f67c5e95feff8d22512d4d5fbe8944a780bdf
Lukas Küffer <lukas.kueffer@outlook.com>
2022-09-17 08:28:52 +0200

    Nice line there guy

--- HackZurichApp/src/main/java/ch/hackzurich/trains/MainActivity.kt
+++ HackZurichApp/src/main/java/ch/hackzurich/trains/MainActivity.kt
@@ -1,19 +1,14 @@
 package ch.hackzurich.trains
 
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
-import org.apache.commons.math3.fitting.PolynomialCurveFitter
-import org.apache.commons.math3.fitting.WeightedObservedPoints
+import androidx.appcompat.app.AppCompatActivity
 import org.opencv.android.*
-import org.opencv.android.OpenCVLoader
 import org.opencv.core.*
-
 import org.opencv.core.Core.*
 import org.opencv.imgproc.Imgproc.*
-import kotlin.math.PI
+import java.lang.Math.PI
 
 
 class MainActivity : AppCompatActivity() {
@@ -38,12 +33,23 @@ class MainActivity : AppCompatActivity() {
         val cannyImage = Mat()
         Canny(blurredImage, cannyImage, 50.0, 150.0)
 
-        // var lineImage = Mat()
-        // HoughLinesP(cannyImage, lineImage, 1.0, PI/180, 50, 20.0, 20.0)
+        var lineImage = Mat()
+        HoughLinesP(cannyImage, lineImage, 1.0, PI/180, 50, 20.0, 20.0)
 
-        val bitmapImage = Bitmap.createBitmap(cannyImage.cols(), cannyImage.rows(), Bitmap.Config.ARGB_8888)
+        for (x in 0 until lineImage.cols()) {
+            val vec: DoubleArray = lineImage.get(0, x)
+            val x1 = vec[0]
+            val y1 = vec[1]
+            val x2 = vec[2]
+            val y2 = vec[3]
+            val start = Point(x1, y1)
+            val end = Point(x2, y2)
+            line(resizedImage, start, end, Scalar(255.0, 0.0, 0.0), 3)
+        }
 
-        Utils.matToBitmap(cannyImage, bitmapImage)
+        val bitmapImage = Bitmap.createBitmap(resizedImage.cols(), resizedImage.rows(), Bitmap.Config.ARGB_8888)
+
+        Utils.matToBitmap(resizedImage, bitmapImage)
 
         mainImageView.setImageBitmap(bitmapImage)
     }

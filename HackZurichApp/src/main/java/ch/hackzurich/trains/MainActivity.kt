0a2f6b3cad1f1973c3fd484c737f307e849c97c1
needToRoll <fb.buergi@gmx.ch>
2022-09-17 04:05:20 +0200

    add static image

--- HackZurichApp/src/main/java/ch/hackzurich/trains/MainActivity.kt
+++ HackZurichApp/src/main/java/ch/hackzurich/trains/MainActivity.kt
@@ -3,11 +3,24 @@ package ch.hackzurich.trains
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
+import org.opencv.core.CvType
+import org.opencv.core.Mat
+import java.io.IOException
+import java.sql.Types
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         OpenCVLoader.initDebug()
+
+        var img: Mat? = null
+        try {
+            img = Utils.loadResource(this, R.drawable.movie_moment, CvType.CV_8UC4)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
     }
 }
---
+++ app/src/main/java/ch/hackzurich/trains/model/TrainOutline.kt
@@ -0,0 +1,2 @@
+package ch.hackzurich.trains.model
+
